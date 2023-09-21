6b59b76711759adf447a6cd1da01ab221f565b2c
sefakor20 <sefakorhom2012@gmail.com>
2023-09-21 20:48:56 +0000

    add a form to search staff by their unique Id

---
+++ app/Livewire/Staff/Search.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Staff;
+
+use Livewire\Component;
+
+class Search extends Component
+{
+    public function render()
+    {
+        return view('livewire.staff.search');
+    }
+}
